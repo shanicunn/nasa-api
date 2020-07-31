5bd7e4ddf2670832d9c5640dcd9e445f8a98a84c
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-31 11:02:44 -0400

    Router set up and installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import APOD from './components/APOD';
+import OldAPODS from './components/OldAPODS';
+import Sources from './components/Sources';
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,12 +10,46 @@ import {
   Link
 } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <APOD />
-    </div>
+class App extends React.Component {
+  componentDidMount() {
+    console.log("Mounted")
+  }
+  render() {
+    return (
+      <Router>
+      <div className="child nav">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">HOME</Link>
+            </li>
+            <li>
+              <Link to="/oldApod">PAST APODS</Link>
+            </li>
+            <li>
+              <Link to="/sources">SOURCES</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/home">
+            <APOD />
+          </Route>
+          <Route path="/oldApod">
+            <OldAPODS />
+          </Route>
+          <Route path="/sources">
+            <Sources />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+  }
+  
 }
 
 export default App;
