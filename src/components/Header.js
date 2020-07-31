291cba8e0c919b2d6e9932a07632c60d4207ea2c
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-31 14:36:00 -0400

    From I am the header to Shani's Astrology Photo Of The Day

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
     return (
         <div className="child header">
-            <h1>I am the header</h1>
+            <h1>Shani's Astrology Photo Of The Day</h1>
         </div>
     )
 }
