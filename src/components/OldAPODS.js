048b39cd5ed7c59953fdbc723a15e6ce181f2c45
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-31 13:02:52 -0400

    Trying to reflect userDate change in api url

--- src/components/OldAPODS.js
+++ src/components/OldAPODS.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import ViewAPOD from './ViewAPOD';
+// import Input from './Input';
 
 const BASE_URL = "https://api.nasa.gov/planetary/apod?api_key=M0Q1d8CExIBLjGoFC0Stdn2ZC0OtYK7Dlo2KFqBd";
 
@@ -9,18 +11,27 @@ class OldAPODS extends React.Component {
 
         this.state = {
             info: [],
-            date: "",
+            userDate: "",
+            formCompleted: false,
         };
     }
     onDateChange = (e) => {
         this.setState({
-          date: e.target.value,
+          userDate: e.target.value,
         })
+
       }
+    //   handleFormSubmission = (e) => {
+    //     e.preventDefault()
+    //     this.setState({
+    //       formCompleted: true,
+    //     })
+    //   }
     async getOldAPODS() {
         try {
-            const res = await axios.get(BASE_URL + this.state.date);
+            const res = await axios.get(`${BASE_URL}&date=${this.state.userDate}`);
             console.log(res);
+            console.log(`${BASE_URL}&date=${this.state.userDate}`)
             this.setState(
                 { info: res.data }
             )
@@ -35,25 +46,23 @@ class OldAPODS extends React.Component {
         return (
 
             <div className="child pastApods">
-                <form className="form">
+               <div className="form">
+                   {/* <Input /> */}
+                   <form className="form">
                     <label>What date would you like to see (YYYY-MM-DD):</label> 
                     <br></br>
                     <input type="text" placeholder="YYYY-MM-DD" 
-                        value={this.state.date} onChange={this.onDateChange}
+                        value={this.state.userDate} onChange={this.onDateChange}
+                    />
+                    <ViewAPOD 
+                        
                     />
                 </form>
-                
-                {/* <div className="date">
-                    <h1>{this.state.info.date}</h1>
-                </div>
-                <div className="photo">
-                    <img src={this.state.info.url} alt=""></img>
-                </div>
-                <div className="explanation">
-                    <p>{this.state.info.explanation}</p>
-                </div> */}
+               </div>
+               
+               
                 <div className="oldApods">
-                    <button type="submit">Check Out A Different Date</button>
+                    <button type="submit" onClick={this.onDateChange}>Check Out A Different Date</button>
                 </div>
             </div>
         )
