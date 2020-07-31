2ea4e40c1caa6e9c613d6e7bc9b9fe33069549f0
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-31 11:32:54 -0400

    Content template added

---
+++ src/components/OldAPODS.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import axios from 'axios';
+
+const BASE_URL = "https://api.nasa.gov/planetary/apod?api_key=M0Q1d8CExIBLjGoFC0Stdn2ZC0OtYK7Dlo2KFqBd";
+
+class OldAPODS extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            info: [],
+        };
+    }
+    async getOldAPODS() {
+        try {
+            const res = await axios.get(BASE_URL);
+            console.log(res);
+            this.setState(
+                { info: res.data }
+            )
+        } catch {
+
+        }
+    }
+    componentDidMount() {
+        this.getOldAPODS();
+    }
+    render() {
+        return (
+
+            <div className="child pastApods">
+                <div className="date">
+                    <h1>{this.state.info.date}</h1>
+                </div>
+                <div className="photo">
+                    <img src={this.state.info.url} alt=""></img>
+                </div>
+                <div className="explanation">
+                    <p>{this.state.info.explanation}</p>
+                </div>
+                <div className="oldApods">
+                    <button>Check Out A Different Date</button>
+                </div>
+            </div>
+        )
+    }
+}
+export default OldAPODS;
