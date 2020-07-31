e7ba5b5e1617b816e4d0c6fb1df1d4e131d3b957
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-31 15:13:10 -0400

    Created list for sources

---
+++ src/components/Sources.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Sources() {
+    return (
+        <div className="child sources">
+            <h1>Sources:</h1>
+            <ul>
+                <li> </li>
+            </ul>
+        </div>
+    )
+}
+export default Sources;
