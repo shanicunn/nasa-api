f56e5497cdb1ca42889e53d66855af26ce93612f
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-03 17:31:02 -0400

    Added comments to explain the workings

--- src/components/APOD.js
+++ src/components/APOD.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
+// Base url with api key
 const BASE_URL = "https://api.nasa.gov/planetary/apod?api_key=M0Q1d8CExIBLjGoFC0Stdn2ZC0OtYK7Dlo2KFqBd";
 
 class APOD extends React.Component {
@@ -11,6 +12,7 @@ class APOD extends React.Component {
             info: [],
         };
     }
+    // Asynchronous method to retireve APOD api
     async getAPOD() {
         try {
             const res = await axios.get(BASE_URL);
@@ -22,11 +24,14 @@ class APOD extends React.Component {
 
         }
     }
+    // Check APOD is running
     componentDidMount() {
+        // console.log("APOD did mount");
         this.getAPOD();
     }
     render() {
         return (
+            // Displays date, title, photo, image and button to see old APODs
             <div className="child apod">
                 <div className="date">
                     <h1>{this.state.info.date}</h1>
