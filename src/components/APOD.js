4390aa87e6109460e31f7564a2418c89067229ef
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-04 09:20:44 -0400

    OldButton state and component added

--- src/components/APOD.js
+++ src/components/APOD.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import OldButton from './OldButton';
+import OldAPODS from './OldAPODS';
 
 // Base url with api key
 const BASE_URL = "https://api.nasa.gov/planetary/apod?api_key=M0Q1d8CExIBLjGoFC0Stdn2ZC0OtYK7Dlo2KFqBd";
@@ -10,8 +12,18 @@ class APOD extends React.Component {
 
         this.state = {
             info: [],
+            isEmptyState: true 
         };
     }
+    // https://flaviocopes.com/react-show-different-component-on-click/
+    // Button function
+    triggerAddTripState = () => {
+        this.setState({
+        ...this.state,
+        isEmptyState: false,
+        isOldButtonState: true
+    })
+  }
     // Asynchronous method to retireve APOD api
     async getAPOD() {
         try {
@@ -46,7 +58,8 @@ class APOD extends React.Component {
                     <p>{this.state.info.explanation}</p>
                 </div>
                 <div className="oldApods">
-                    <button>Check Out Past Dates</button>
+                {this.state.isEmptyState && <OldButton oldbutton={this.triggerAddTripState}/>}
+                {this.state.isOldButtonState && <OldAPODS />}
                 </div>
             </div>
         )
