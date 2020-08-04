4ca175a550b38a45793a00dade25e82c92485bac
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-04 09:35:19 -0400

    OldButton component coded correctly

--- src/components/APOD.js
+++ src/components/APOD.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import OldButton from './OldButton';
-import OldAPODS from './OldAPODS';
+// import OldAPODS from './OldAPODS';
 
 // Base url with api key
 const BASE_URL = "https://api.nasa.gov/planetary/apod?api_key=M0Q1d8CExIBLjGoFC0Stdn2ZC0OtYK7Dlo2KFqBd";
@@ -12,20 +12,20 @@ class APOD extends React.Component {
 
         this.state = {
             info: [],
-            isEmptyState: true 
+            // isEmptyState: true ,
         };
     }
     // https://flaviocopes.com/react-show-different-component-on-click/
     // Button function
-    triggerAddTripState = () => {
-        this.setState({
-        ...this.state,
-        isEmptyState: false,
-        isOldButtonState: true
-    })
-  }
+//     triggerAddTripState = () => {
+//         this.setState({
+//         ...this.state,
+//         isEmptyState: false,
+//         isOldButtonState: true,
+//     })
+//   }
     // Asynchronous method to retireve APOD api
-    async getAPOD() {
+    async getAPOD  () {
         try {
             const res = await axios.get(BASE_URL);
             console.log(res);
@@ -37,7 +37,7 @@ class APOD extends React.Component {
         }
     }
     // Check APOD is running
-    componentDidMount() {
+    componentDidMount = () => {
         // console.log("APOD did mount");
         this.getAPOD();
     }
@@ -58,8 +58,9 @@ class APOD extends React.Component {
                     <p>{this.state.info.explanation}</p>
                 </div>
                 <div className="oldApods">
-                {this.state.isEmptyState && <OldButton oldbutton={this.triggerAddTripState}/>}
-                {this.state.isOldButtonState && <OldAPODS />}
+                {/* {this.state.isEmptyState && <OldButton oldbutton={this.triggerAddTripState}/>}
+                {this.state.isAddTripState && <OldAPODS />} */}
+                <OldButton  />
                 </div>
             </div>
         )
