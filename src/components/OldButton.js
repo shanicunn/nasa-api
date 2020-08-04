2a22ad2fef44dbe9a18d6035d6ef8d8c1deea4c0
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-04 09:21:01 -0400

    Component created for OldButton

---
+++ src/components/OldButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function OldButton() {
+    return (
+        <div>
+            <button onClick={props.oldButton} >Check Out Past Dates</button>
+        </div>
+    )
+}
+export default OldButton;
