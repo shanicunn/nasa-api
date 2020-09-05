19381906c4a17fc81ec0b036f0e54c74217534ed
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-09-04 21:30:22 -0400

    Specification of Angel resource

--- src/components/Sources.js
+++ src/components/Sources.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Sources() {
     return (
-        <div className="child sources">
+        <div className="child sources border">
             <h1>Sources:</h1>
             <ul>
                 <li> <a href="https://api.nasa.gov/">NASA's APOD API</a></li>
-                <li>Angel</li>
+                <li>Angel (My instructor)</li>
                 <li><a href="https://github.com/ReactTraining/react-router/issues/1176">React Link with components</a></li>
                 <li><a href="https://reactgo.com/reactrouter/navlink/">Styling for active links</a></li>
                 <li><a href="https://cssgradient.io/blog/css-gradient-text/">Gradient color</a></li>
